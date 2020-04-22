b9ed5936e35d718d1a2137d6a51478ea1bac79cc
Michal <mich.krzyzanowski@gmail.com>
2020-04-22 09:58:13 +0200

    Added testng

---
+++ src/test/java/com/capgemini/tests/TestngPlayground.java
@@ -0,0 +1,11 @@
+package com.capgemini.tests;
+
+import org.testng.annotations.Test;
+
+public class TestngPlayground {
+
+  @Test
+  public void firstTest() {
+    System.out.println("DOES IT WORK?!");
+  }
+}
