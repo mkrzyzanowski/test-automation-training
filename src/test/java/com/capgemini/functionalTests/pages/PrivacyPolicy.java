237956481899a21d51b24ac5dcd0bb412eec608a
Michal <mich.krzyzanowski@gmail.com>
2020-04-24 07:42:55 +0200

    Logback added

--- src/test/java/com/capgemini/functionalTests/pages/PrivacyPolicy.java
+++ src/test/java/com/capgemini/functionalTests/pages/PrivacyPolicy.java
@@ -4,10 +4,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import io.qameta.allure.Step;
 
 public class PrivacyPolicy {
+  private static final Logger LOG = LoggerFactory.getLogger(PrivacyPolicy.class);
 
   @FindBy(css = "h1")
   private WebElement heading1;
@@ -22,6 +25,7 @@ public class PrivacyPolicy {
   @Step("Opening Privacy Policy page!")
   public void open() {
     driver.get("https://automationintesting.online/#/privacy");
+    LOG.info("Visiting the Privacy Policy page");
   }
 
   @Step("Retrieving heading text")
